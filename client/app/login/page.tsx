6810f8e303afc4b7470ed261796d726e545ce3e6
Ranjithp07 <ranjithp27122003@gmail.com>
2026-03-22 09:42:43 +0530

    navigation issue fixed

--- client/app/login/page.tsx
+++ client/app/login/page.tsx
@@ -48,7 +48,7 @@ export default function LoginPage() {
       }
 
       setSuccess("Login successful!");
-      router.push(userRole === "SELLER" ? "/profile" : "/dashboard");
+      router.push(userRole === "CREATOR" ? "/profile" : "/dashboard");
     } catch (err: unknown) {
       if (err instanceof Error) {
         setError(err.message || "Login failed");
